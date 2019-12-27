fb1bffabbb951f0e54a15da2776556a672e7e65e
Ryan Mehri <ryan.mehri1@gmail.com>
2019-12-26 17:15:22 -0700

    Add test for predicting temp and fix radians bug

--- observatory/src/test/scala/observatory/VisualizationTest.scala
+++ observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -1,12 +1,18 @@
 package observatory
 
-import org.junit.Assert._
-import org.junit.Test
+import observatory.Visualization._
+import org.junit._
 
 trait VisualizationTest extends MilestoneSuite {
   private val milestoneTest = namedMilestoneTest("raw data display", 2) _
 
-  // Implement tests for the methods of the `Visualization` object
-
+  @Test def `predict temp`(): Unit = {
+    val t1 = (Location(0 , 0), 0d)
+    val t2 = (Location(39 , 60), 20d)
+    val temps = Seq(t1, t2)
+    val predict = Location(10, 10)
+    val result = predictTemperature(temps, predict)
+    assert(result > 0 && result < 10) // closer to t1
+  }
 
 }
