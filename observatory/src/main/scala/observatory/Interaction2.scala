865160d80d88134490c01f4f6035ab376824b775
Ryan Mehri <ryan.mehri1@gmail.com>
2019-12-28 15:17:33 -0700

    Complete final milestone

--- observatory/src/main/scala/observatory/Interaction2.scala
+++ observatory/src/main/scala/observatory/Interaction2.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import observatory.LayerName.{Deviations, Temperatures}
+
 /**
   * 6th (and last) milestone: user interface polishing
   */
@@ -9,7 +11,29 @@ object Interaction2 extends Interaction2Interface {
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    val tempColors = Seq(
+      (60d, Color(255, 255, 255)),
+      (32d, Color(255, 0, 0)),
+      (12d, Color(255, 255, 0)),
+      (0d, Color(0, 255, 255)),
+      (-15d, Color(0, 0, 255)),
+      (-27d, Color(255, 0, 255)),
+      (-50d, Color(33, 0, 107)),
+      (-60d, Color(0, 0, 0))
+    )
+    val deviationColors = Seq(
+      (7d, Color(0, 0, 0)),
+      (4d, Color(255, 0, 0)),
+      (2d, Color(255, 255, 0)),
+      (0d, Color(255, 255, 255)),
+      (-2d, Color(0, 255, 255)),
+      (-7d, Color(0, 0, 255))
+    )
+
+    Seq(
+      Layer(Temperatures, tempColors, 1975 to 2015),
+      Layer(Deviations, deviationColors, 1975 to 2015)
+    )
   }
 
   /**
@@ -17,37 +41,41 @@ object Interaction2 extends Interaction2Interface {
     * @return A signal containing the year bounds corresponding to the selected layer
     */
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
+    Signal(selectedLayer().bounds)
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param sliderValue The value of the year slider
+    * @param sliderValue   The value of the year slider
     * @return The value of the selected year, so that it never goes out of the layer bounds.
     *         If the value of `sliderValue` is out of the `selectedLayer` bounds,
     *         this method should return the closest value that is included
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
+    val currSliderValue = sliderValue()
+    val bound = yearBounds(selectedLayer)()
+    val clampedYear = currSliderValue.max(bound.min).min(bound.max)
+
+    Signal(clampedYear)
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"target/${selectedLayer().layerName.id}/${selectedYear()}/{z}/{x}/{y}.png")
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"${selectedLayer().layerName.id.capitalize} (${selectedYear()})")
   }
 
 }
@@ -56,22 +84,30 @@ object Interaction2 extends Interaction2Interface {
 // or your submission will fail with a NoSuchMethodError.
 trait Interaction2Interface {
   def availableLayers: Seq[Layer]
+
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range]
+
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year]
+
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String]
+
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String]
 }
 
 sealed abstract class LayerName(val id: String)
+
 object LayerName {
+
   case object Temperatures extends LayerName("temperatures")
+
   case object Deviations extends LayerName("deviations")
+
 }
 
 /**
-  * @param layerName Name of the layer
+  * @param layerName  Name of the layer
   * @param colorScale Color scale used by the layer
-  * @param bounds Minimum and maximum year supported by the layer
+  * @param bounds     Minimum and maximum year supported by the layer
   */
 case class Layer(layerName: LayerName, colorScale: Seq[(Temperature, Color)], bounds: Range)
 
